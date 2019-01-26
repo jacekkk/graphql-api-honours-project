bc587bf61c8e8cad9faeb5d23f1af1b54ef0c1ff
JacasEdi <jacek.budzynski.92@gmail.com>
2019-01-26 13:28:57 +0000

    Add Apollo Engine config to allow Heroku deployment

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const { ApolloEngine } = require("apollo-engine");
+const { ApolloServer } = require("apollo-server-express");
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const mongoose = require('mongoose');
-
 const graphQLSchema = require('./graphql/schema/index');
 const graphQLResolvers = require('./graphql/resolvers/index');
+
+
+const server = new ApolloServer({
+    graphQLSchema,
+    graphQLResolvers,
+    tracing: true,
+    cacheControl: true,
+    engine: false
+  });
 
 const app = express();
 
@@ -16,9 +26,19 @@ app.use('/graphql', graphqlHttp({
     graphiql: true
 }));
 
+server.applyMiddleware({ app });
+
+const engine = new ApolloEngine({
+    apiKey: "service:jacas-6348:ju8vT9lCxKhvC8gQ98LPwg"
+});
+
 mongoose.connect(`mongodb+srv://jacas:w97LPKw77AgP2Usj@cluster0-vvdc2.mongodb.net/honours-project?retryWrites=true`
 ).then(() => {
-    app.listen(3000);
+    // app.listen(3000);
+    engine.listen({
+        port: 3000,
+        expressApp: app
+    });
 }).catch(err => {
     console.log(err);
 });
